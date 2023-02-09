7dbbe9bcb03ceebc42c845ad467d5eac0df9ca8a
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-09 18:41:06 +0530

    making models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Song
 # Register your models here.
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display=['name','duration']
--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Song(models.Model):
+    user=models.ManyToManyField(User)
+    name=models.CharField(max_length=30)
+    duration=models.IntegerField()
